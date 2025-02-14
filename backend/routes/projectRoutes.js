c1fcfae1f4409337b3d02628148ed3249b4800ce
Abdirashiid bile <abdirashiidbile5@gmail.com>
2025-02-14 15:32:56 +0300

    fixied error

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getProjects, createProject } = require('../controllers/projectController');
+const { getProjects, createProject, updateProject, deleteProject } = require('../controllers/projectController');
 
 const router = express.Router();
 
 router.get('/', getProjects);
 router.post('/', createProject);
+router.put('/:id', updateProject); // PUT route for updating a project
+router.delete('/:id', deleteProject); // DELETE route for deleting a project
 
 module.exports = router;
